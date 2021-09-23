462ff3ac98777df42e96b30a965f6424a6c48afa
chjimenezr <chjimenezr@gmail.com>
2021-09-23 17:23:10 -0500

    base proyecto react

---
+++ consultools/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>MinTic</p>
+        <p>
+        Universidad de antioquia
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
